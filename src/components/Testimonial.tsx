500a5bf7f82c4e00949d2704bde73944fa144f87
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-04-27 17:01:33 +0700

    Add Testimonial component to Home pageThe commit adds a Testimonial component to the Home page, which displays quotes from customers and their photos. This enhances the credibility of the website and builds trust with potential customers

---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,119 @@
+export default function Testimonial() {
+  return (
+    <section className="bg-gray-100 text-gray-800">
+      <div className="container mx-auto px-6 py-12">
+        <div className="grid items-center gap-4 xl:grid-cols-5">
+          <div className="mx-auto my-8 max-w-2xl space-y-4 text-center xl:col-span-2 xl:text-left">
+            <h2 className="text-4xl font-bold">
+              Duo assum utroque appetere an
+            </h2>
+            <p className="text-gray-600">
+              Pri ex magna scaevola moderatius. Nullam accommodare no vix, est
+              ei diceret alienum, et sit cetero malorum. Et sea iudico
+              consequat, est sanctus adipisci ex.
+            </p>
+          </div>
+          <div className="p-6 xl:col-span-3">
+            <div className="grid gap-4 md:grid-cols-2">
+              <div className="grid content-center gap-4">
+                <div className="rounded bg-gray-50 p-6 shadow-md">
+                  <p>
+                    An audire commodo habemus cum. Ne sed corrumpit repudiandae.
+                    Tota aliquip democritum pro in, nec democritum intellegam
+                    ne. Propriae volutpat dissentiet ea sit, nec at lorem inani
+                    tritani, an ius populo perfecto vituperatoribus. Eu cum case
+                    modus salutandi, ut eum vocent sensibus reprehendunt.
+                  </p>
+                  <div className="mt-4 flex items-center space-x-4">
+                    <img
+                      src="https://source.unsplash.com/50x50/?portrait?1"
+                      alt=""
+                      className="h-12 w-12 rounded-full bg-gray-500 bg-cover bg-center"
+                    />
+                    <div>
+                      <p className="text-lg font-semibold">Leroy Jenkins</p>
+                      <p className="text-sm text-gray-600">
+                        CTO of Company Co.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="rounded bg-gray-50 p-6 shadow-md">
+                  <p>
+                    Sit wisi sapientem ut, pri civibus temporibus voluptatibus
+                    et, ius cu hinc fabulas. Nam meliore minimum et, regione
+                    convenire cum id. Ex pro eros mucius consectetuer, pro magna
+                    nulla nonumy ne, eam putent iudicabit consulatu cu.
+                  </p>
+                  <div className="mt-4 flex items-center space-x-4">
+                    <img
+                      src="https://source.unsplash.com/50x50/?portrait?2"
+                      alt=""
+                      className="h-12 w-12 rounded-full bg-gray-500 bg-cover bg-center"
+                    />
+                    <div>
+                      <p className="text-lg font-semibold">Leroy Jenkins</p>
+                      <p className="text-sm text-gray-600">
+                        CTO of Company Co.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="grid content-center gap-4">
+                <div className="rounded bg-gray-50 p-6 shadow-md">
+                  <p>
+                    Putant omnium elaboraret per ut. Id dicta tritani nominavi
+                    quo, mea id justo errem elaboraret. Agam mollis scripserit
+                    ea his, ut nec postea verear persecuti. Ea noster senserit
+                    eam, ferri omittantur ei nec. Id mel solet libris
+                    efficiantur, commune explicari et eos. Case movet ad est,
+                    sed tota vocent appetere ea.
+                  </p>
+                  <div className="mt-4 flex items-center space-x-4">
+                    <img
+                      src="https://source.unsplash.com/50x50/?portrait?3"
+                      alt=""
+                      className="h-12 w-12 rounded-full bg-gray-500 bg-cover bg-center"
+                    />
+                    <div>
+                      <p className="text-lg font-semibold">Leroy Jenkins</p>
+                      <p className="text-sm text-gray-600">
+                        CTO of Company Co.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="rounded bg-gray-50 p-6 shadow-md">
+                  <p>
+                    Te omnes virtute volutpat sed. Ei esse eros interesset vel,
+                    ei populo denique ocurreret vix, eu cum pertinax mandamus
+                    vituperatoribus. Solum nihil luptatum per ex, ei amet
+                    viderer eos. Ea illum labitur mnesarchum pro. Eius meis
+                    salutandi ei nam, alterum expetenda et nec. Expetenda
+                    intellegat at eum, per mazim sanctus honestatis ad. Ei
+                    noluisse invenire vix. Te ancillae patrioque qui, probo
+                    bonorum vivendum ex vim.
+                  </p>
+                  <div className="mt-4 flex items-center space-x-4">
+                    <img
+                      src="https://source.unsplash.com/50x50/?portrait?4"
+                      alt=""
+                      className="h-12 w-12 rounded-full bg-gray-500 bg-cover bg-center"
+                    />
+                    <div>
+                      <p className="text-lg font-semibold">Leroy Jenkins</p>
+                      <p className="text-sm text-gray-600">
+                        CTO of Company Co.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
