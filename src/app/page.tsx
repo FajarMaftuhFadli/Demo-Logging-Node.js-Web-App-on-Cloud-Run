8d397c285d1d4bb6b40f6487156567bf1a5dd150
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-04-28 12:36:15 +0700

    Remove unused imports and font declaration in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import Hero from '@/components/Hero';
 import Blog from '@/components/Blog';
 import Testimonial from '@/components/Testimonial';
 import Features from '@/components/Features';
 import Stats from '@/components/Stats';
 import CallToAction from '@/components/CallToAction';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
